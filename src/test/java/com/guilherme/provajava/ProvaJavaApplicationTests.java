68a36252422baa2a6c12d374ab7f4b33fdb2db14
darkgx207 <guilhermerocha207@gmail.com>
2024-04-30 18:20:32 -0300

    Project creation

---
+++ src/test/java/com/guilherme/provajava/ProvaJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.provajava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
